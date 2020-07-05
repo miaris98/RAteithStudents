b467ed4f28e74718dcfe92e622adeef3e56e5698
dennis <39520771+miaris98@users.noreply.github.com>
2020-07-05 17:29:36 +0300

    Update EksetaseisR.R

--- Code/EksetaseisR.R
+++ Code/EksetaseisR.R
@@ -5,14 +5,13 @@ rm(list=ls())
 
 # load libraries
 library(readxl)
-# library(stringr)
 library(dplyr)
 
 
 # =====================================================================
-#                               Import the data stringsAsFactors = FALSE,read.table(filname.txt, row.names=1),headers einai ta labels
+#                               Import the data 
 # =====================================================================
-Data <- read.csv(file ="Data.csv", fileEncoding = "UTF-8-BOM", sep=",")
+Data <- read.csv(file ="CSV/Data.csv", fileEncoding = "UTF-8-BOM", sep=",")#maybe u need to change the path to the file
 View(Data)
 levels(Data$course)
 selected <- filter(Data,Data$year==2018 & Data$exam=="S1",Data$course=="C4")
