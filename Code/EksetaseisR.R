1d6ead88d1a6846cd25883adca4bf6a2d4c9d0d8
dennis <39520771+miaris98@users.noreply.github.com>
2020-07-05 21:52:36 +0300

    Add files via upload
    added percentage that passed per class

--- Code/EksetaseisR.R
+++ Code/EksetaseisR.R
@@ -2,11 +2,12 @@
 
 
 rm(list=ls())
-
+library(data.table)
 # load libraries
 library(readxl)
 # library(stringr)
 library(dplyr)
+library(ISLR)
 
 
 # =====================================================================
@@ -26,13 +27,54 @@ Summary_Of_Students_In_2018_S1_C4<-summary(selected)
 Data_Frame_of_Summary<-as.data.frame.matrix(Summary_Of_Students_In_2018_S1_C4)#converting to dataframe so i can use pull function(Note : it returns vectors) 
 
 grade_of_that_exam<-pull(Data_Frame_of_Summary, var = -1)#i am getting the last column
-View(grade_of_that_exam)
+#View(grade_of_that_exam)
 #=================================== cut function to convert numeric to factor using break in range ==========
 Data_with_grade_bigger_than_5<-filter(Data,Data$grade>5)
 
 Data_with_grade_bigger_than_5$d_grade <- cut(Data_with_grade_bigger_than_5$grade, breaks = c(5,6.25,7.5,8.75,10.1),include.lowest=TRUE,labels = c("medium","good","very good","excellent"), 
                       right = TRUE)
 
-View(Data_with_grade_bigger_than_5)
-#================================================================================================================
+#View(Data_with_grade_bigger_than_5)
+#================================================================================================================metrw tous ba8mous >5 ana taksh kai diarw ana taksh me tous sunolikous ba8mous===
+Data_with_varaiety_in_classes<-filter(Data,Data$year==2018 & Data$exam=="S1")
+Data_with_varaiety_in_classes$d_grade <- cut(Data_with_varaiety_in_classes$grade, breaks = c(0,5,6.25,7.5,8.75,10.1),include.lowest=TRUE,labels = c("failed","medium","good","very good","excellent"), 
+                   right = TRUE)
+View(Data_with_varaiety_in_classes)
 
+#===Failed attempts====
+#count_row_if(Data_with_varaiety_in_classes$d_grade>5)
+#table(Data_with_varaiety_in_classes)
+
+
+#aggregate(x = Data_with_varaiety_in_classes$d_grade,
+#          by = list(Data_with_varaiety_in_classes$course),
+#          FUN=sum)
+#====================================
+table_data<-tbl_df(Data_with_varaiety_in_classes)#convert dataframe to table
+View(table_data)
+#effective group by using %>% which it does "pipe/merge" functions together
+results<-table_data %>%
+  group_by(course)%>%
+  summarize(percentage_passed = sum(grade>5, na.rm = TRUE)/sum(grade,na.rm = TRUE)*100)
+View(results)
+
+
+
+
+
+
+
+
+
+
+
+grouped_with_class<-group_by(Data_with_varaiety_in_classes,Data_with_varaiety_in_classes$course)
+grouped_with_class$percetance_passed<-sum(Data_with_varaiety_in_classes$grade>5)/Data_with_varaiety_in_classes$grade 
+View(grouped_with_class)
+
+selected%>%
+  group_by(course)%>%
+  summarise(sum(selected$grade>5))%>%
+  summarise((sum(selected$grade>5)/selected$grade))%>%
+  mutate(percent_calling=selected$grade/sum(selected$grade)*100)%>%
+  select(-selected$selected$grade) # remove the sum if not required
